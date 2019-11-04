9cd1b9c829cd94a14336ed7e41bdd8012f811dc7
yayatwane <yayatwane@hotmail.fr>
2019-11-04 15:30:38 +0100

    Significant Update
    - Generator pick according to order
    - Update of fonctions.py
    - Decryption bug correction

--- ECC_two_bl.py
+++ ECC_two_bl.py
@@ -5,21 +5,27 @@ import random
 import math
 from itertools import product
 import binascii
-from fonctions import n_premiers, hexadeci, deci, euclide_etendu, crypter, decrypter
+from fonctions import n_premiers, hexadeci, deci, euclide_etendu, crypter, decrypter, calc_2alpha, calc_o_alpha, calc_n_alpha, calc_G, calc_order_Gen, best_generator
 
 listex=[]
 listey=[]
 a=4
 b=1
 lambd=0
-modulo=1321
+modulo=1297
 temp=0
 i=1
-kerr=2
+kerr=2 # Taille des blocs unitaires a coder
+best_gen=[]
 # Calcul du Nombre de blocs
 caracteres=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
 nchar=len(caracteres)
 bl_size=nchar**2
+Gen=calc_G(a,b,modulo)
+best_gen=best_generator(Gen,a,b,modulo,bl_size)
+print('best_gen*********************************************************************',best_gen)
+G=best_gen
+
 
 up = bl_size + 50
 n_premiers(bl_size,up)
@@ -33,21 +39,10 @@ combs = hexadeci(combs)
 
 
 
-""" ETAPE 0 : CHOIX D'UN GENERATEUR """
-while temp!=1:
-	x=i;
-	res=((x**3)+(a*x)+b)%modulo
-	racine=math.sqrt(res)
-	if racine.is_integer():
-		y=int(racine)	
-		temp=1
-	else:
-		i=i+1	
-#print(x,y)
-G=[x,y]
+# G=(1296,1225)
 print("\n \n Notre generateur est ",G)
-x1=x
-y1=y
+x1=G[0]
+y1=G[1]
 
 
 listex.append(x1)
@@ -57,7 +52,7 @@ ordre=1
 
 """ ETAPE 1 : DEFINITION DE (x3,x3) """
 
-val=((3*x1**2)+2)%modulo
+val=((3*x1**2)+a)%modulo
 #phi_n=37
 e=2*y1%modulo
 
@@ -92,7 +87,8 @@ while tempo!=1:
 		ordre=ordre+1
 #print(listex)
 #print(listey)
-print("Nombre de points :",len(listex))
+n_points=len(listex)
+print("Nombre de points :",n_points)
 #print(len(listey))
 
 
@@ -100,7 +96,7 @@ couple = []
 for x,y in zip(listex,listey):
  a = (x,y)
  couple.append(a)
-#print (couple)
+print (couple)
 ## Cryptage
 k = 3265477 # cle publique ??
 beta = k%modulo
@@ -110,7 +106,9 @@ print(len(texte1))
 result=[]
 indice=0
 message=''
-hexamessage=''
+messagec=''
+hexamessagec='' # Message hexadecimal crypte
+hexamessage='' # Message hexadecimal decrypte
 bonus='y'
 
 toto=0
@@ -132,8 +130,14 @@ while toto!=1:
 
 	point=(couple[nombre]) #equivalent du message dans la courbe elliptique
 
-	crypte = crypter(point,modulo,couple,k)
-	decrypte = decrypter(crypte,modulo,couple,k)
+	crypte = crypter(point,modulo,couple,n_points)
+	indexc = couple.index(crypte)
+	cm=combs[indexc]
+	print(" Le message chiffre est :", cm)
+	hexamessagec = hexamessagec + (str(cm))
+	cm = binascii.unhexlify(cm).decode() # Conversion en chaine de caracteres
+	messagec=messagec+cm
+	decrypte = decrypter(crypte,modulo,couple,n_points)
 	index = couple.index(decrypte)
 	decm=combs[index]
 	print(" Le message dechiffre est :", decm)
@@ -146,10 +150,7 @@ while toto!=1:
 		toto=1
 
 print("Code dechiffre: liste de points ",result)
-#print(crypte)
-#print(decrypte)
+print("Message hexa chiffre:", hexamessagec)
+print("Message chiffre:", messagec)
 print("Message hexa dechiffre:", hexamessage)
 print("Message dechiffre:", message)
-
-
-
--- fonctions.py
+++ fonctions.py
@@ -43,7 +43,6 @@ def euclide_etendu(e, phi_n, val) :
   return d
 
 # Cryptage et Decryptage
-
 def crypter(m, phi_n, tab, coef) :
 	cpt=[]
 	## Cryptage
@@ -63,3 +62,126 @@ def decrypter(m, phi_n, tab, coef) :
 	dcpt=tab[dec]
 	print("code dechiffre:", dcpt)
 	return dcpt
+
+#calcul de 2 alpha
+def calc_2alpha(x1,y1,mod,a):
+	lamda=0	
+	e=2*y1%mod
+	omega = (3*(x1**2)+a) % mod
+	lamda = euclide_etendu(e,mod,omega)
+	x3=(lamda**2-2*x1)%mod
+	y3=(lamda*(x1-x3)-y1)%mod
+	temp=[]
+	temp.append(x3)
+	temp.append(y3)
+	return temp
+
+#calcul des autres alpha a partir de 3 alpha_______________________ 
+def calc_o_alpha(x1,y1,x2,y2,mod,a):
+	lamda=0	
+	e=(x2-x1) % mod
+	omega = (y2-y1) % mod
+	lamda = euclide_etendu(e,mod,omega)
+
+	x3=(lamda**2-x1-x2)%mod
+	y3=(lamda*(x1-x3)-y1)%mod
+
+	temp=[]
+	temp.append(x3)
+	temp.append(y3)
+	return temp
+
+#calcul du generateur____________________________________________________________
+def calc_G(a,b,mod):
+	x=1
+	
+	entier=0
+	counter=0
+	Generator=[]
+	while x<mod:
+		y=1
+		while y<mod:
+			rp=((x**3)+(a*x)+b)%mod
+			lp=(y*y)%mod
+			if rp==lp:
+				counter=counter+1
+				print ("\n( le generateur est ",x,",",y,")")
+				temp=[]
+				if y!=0:
+
+					temp.append(x)
+					temp.append(y)
+					Generator.append(temp)
+			y=y+1
+		x=x+1
+	counter=(counter*2)+1
+	print('counter____________________________________________________________',counter)
+	return Generator
+
+#______________Calcul de n alpha_______________________________________________________
+def calc_n_alpha(G,n,mod,a):
+	n_alpha=[]
+	#all_alpha['a']=G
+	two_alpha=[]
+	current_alpha=[]
+	two_alpha=calc_2alpha(G[0],G[1],mod,a)
+	cur_a=two_alpha
+	if n==2:
+		n_alpha=two_alpha
+	else:
+		i =2	
+		while i<n:
+			cur_a=calc_o_alpha(G[0],G[1],cur_a[0],cur_a[1],mod,a)
+			i=i+1
+		n_alpha=cur_a
+	print('n_alpha________________________________________',n_alpha)
+	return n_alpha
+
+def calc_order_Gen(G,a,b,mod):
+	x1=G[0]
+	y1=G[1]
+	all_alpha={}
+	all_alpha['a']=G
+	current_alpha=[]
+	cur_a=calc_2alpha(G[0],G[1],mod,a)
+	all_alpha['2a']=cur_a
+	i =2
+	while 1:
+		
+		#all_alpha.append(cur_a)
+		# all_alpha[str(i)+'a']=cur_a
+		i=i+1
+		if cur_a[0] == G[0]:
+			#all_alpha.append(cur_a)
+			#print('cur_a___________________1',cur_a)
+			#all_alpha.append(cur_a)
+			break
+		else :
+			cur_a=calc_o_alpha(G[0],G[1],cur_a[0],cur_a[1],mod,a)
+	# print ('all_alpha',all_alpha)
+	print('_________________________________________________________________')
+	# print('ordre de (' + str(G[0])+' , ' + str(G[1]) +') '+str(len(all_alpha)))
+	# print('ordre de (' + str(G[0])+' , ' + str(G[1]) +') '+str(i))
+	# order=len (all_alpha)
+	return i
+	
+def best_generator(Gen,a,b,mod,bs):
+	b_size=0
+	order=0
+	b_gen=[]
+	print('Gen_0__________________________________',Gen[0])
+	b_size=mod+2*(mod**(1/2))+1
+	# b_size=calc_order_Gen(Gen[0],a,b,mod)
+	for t in Gen:
+		order=calc_order_Gen(t,a,b,mod)
+		# print('order__________________________________',order)
+
+		if order>=bs:
+			if order<=b_size:
+				b_size=order
+				# print('b_size__________________________________',b_size)
+				b_gen=t
+	print('bs__________________________________',bs)
+	print('b_size__________________________________',b_size)
+	print('b_gen__________________________________',b_gen)
+	return b_gen
